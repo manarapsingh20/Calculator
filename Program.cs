8efb1d0fe4aeb045f3d751fc2b773d4e64fb3683
manarapsingh20 <150858808+manarapsingh20@users.noreply.github.com>
2023-11-17 13:39:55 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ManarapCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first number:");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" ");
+
+            Console.WriteLine("What operation would you like to use? (+,-,*,/)");
+            String operation = Console.ReadLine();
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Enter your second number:");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Your answer is:");
+
+            if (operation == "+") 
+            {
+                Console.WriteLine(firstNumber + secondNumber);
+            }
+
+            if (operation == "-") 
+            {
+                Console.WriteLine(firstNumber - secondNumber);
+            }
+
+            if (operation == "*") 
+            {
+                Console.WriteLine(firstNumber * secondNumber);
+            }
+
+            if (operation == "/") 
+            {
+                Console.WriteLine(firstNumber / secondNumber);
+            }
+        }
+    }
+}
